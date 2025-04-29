1ee84d3c61201f8dd1ad6cba8c0557088f155b07
AkshitaJ0904 <Akshita.jain.23cse@bmu.edu.in>
2025-04-30 02:07:10 +0530

    Initial commit

--- app/src/main/java/com/example/antisoch/IntroActivity2.java
+++ app/src/main/java/com/example/antisoch/IntroActivity2.java
@@ -6,20 +6,20 @@ import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity2 extends AppCompatActivity {
 
     private Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.activity_intro_2);
 
         btnNext = findViewById(R.id.btnNext);
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(IntroActivity.this, IntroActivity2.class);
+                Intent intent = new Intent(IntroActivity2.this, IntroActivity3.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/antisoch/IntroActivity3.java
+++ app/src/main/java/com/example/antisoch/IntroActivity3.java
@@ -6,20 +6,20 @@ import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity3 extends AppCompatActivity {
 
     private Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        setContentView(R.layout.activity_intro_3);
 
         btnNext = findViewById(R.id.btnNext);
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(IntroActivity.this, IntroActivity2.class);
+                Intent intent = new Intent(IntroActivity3.this, JoinActivity.class);
                 startActivity(intent);
             }
         });
